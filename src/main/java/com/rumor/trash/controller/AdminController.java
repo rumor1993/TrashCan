265edf9ce27e71562c29bdad17e78778153d000b
rumor1993 <rumor1993@naver.com>
2021-12-04 18:56:22 +0900

    [스프링 파일 업로드]

--- src/main/java/com/rumor/trash/controller/AdminController.java
+++ src/main/java/com/rumor/trash/controller/AdminController.java
@@ -1,10 +1,21 @@
 package com.rumor.trash.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class AdminController {
+
+    @Value("${file.dir}")
+    private String fileDir;
 
     @GetMapping("/home")
     public String home() {
@@ -13,6 +24,18 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String admin() {
-        return "admin/admin";
+        return "admin/excel_upload_form.html";
+    }
+
+    @PostMapping("/upload")
+    public String saveFile(@RequestParam String itemName,
+            @RequestParam MultipartFile file) throws IOException {
+        System.out.println("itemName = " + itemName);
+        if (!file.isEmpty()) {
+            String fullPath = fileDir + file.getOriginalFilename();
+            file.transferTo(new File(fullPath));
+        }
+
+        return "redirect:admin/excel_upload_form.html";
     }
 }
